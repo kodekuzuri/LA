d4d353bffbf8fffac3e32ff0af0c0de61ba44754
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2021-10-23 16:27:26 +0530

    Update PlotQ3.py

--- PlotQ3.py
+++ PlotQ3.py
@@ -1,5 +1,22 @@
 #(a)
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
+  
+fig = plt.figure()
+  
+ax1 = fig.add_subplot(111, projection='3d')
+  
+u, v = np.mgrid[0:2*np.pi:200j, 0:np.pi:100j]
+
+x = -(np.cos(u)*np.sin(v)*np.sin(v) + np.cos(u)*np.cos(v)*np.cos(v))/np.sqrt(2)
+y = -(np.sin(u)*np.sin(v)*np.sin(v) + np.sin(u)*np.cos(v)*np.cos(v))/np.sqrt(2)
+z = (np.sin(u)*np.sin(v)*np.sin(v) + np.sin(u)*np.cos(v)*np.cos(v)) - (np.cos(u)*np.sin(v)*np.sin(v) + np.cos(u)*np.cos(v)*np.cos(v)) 
+  
+ax1.plot_wireframe(x, y, z, rstride = 6, cstride = 6, linewidth = 2)
+  
+plt.show()
 
 #(b)
 import numpy as np
